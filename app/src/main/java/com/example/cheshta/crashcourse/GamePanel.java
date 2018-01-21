752aa3170cac28d5280fd147c3317dbdbc462794
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-01-21 22:58:37 +0530

    game loop

---
+++ app/src/main/java/com/example/cheshta/crashcourse/GamePanel.java
@@ -0,0 +1,61 @@
+package com.example.cheshta.crashcourse;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by chesh on 1/21/2018.
+ */
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
+
+    private MainThread thread;
+
+    public GamePanel(Context context) {
+        super(context);
+
+        getHolder().addCallback(this);
+        thread = new MainThread(getHolder(),this);
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        thread = new MainThread(getHolder(),this);
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        boolean retry = true;
+        while (retry){
+            try{
+                thread.setRunning(false);
+                thread.join();
+            } catch (Exception e){ e.printStackTrace(); }
+            retry = false;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    public void update(){
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+    }
+}
---
+++ app/src/main/java/com/example/cheshta/crashcourse/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.cheshta.crashcourse;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new GamePanel(this));
+    }
+}
